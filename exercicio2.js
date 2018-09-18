18cdb9ce1af37e4be8340438d12ce5e624ff0d98
Daniel Gonçalves <d.aguiargoncalves@gmail.com>
2018-09-18 11:33:51 -0300

    exercicio2

--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,44 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+document.querySelectorAll('#anterior').forEach(function(el) {
+   el.addEventListener('click', function(e) {
+   		arrayImg = document.querySelector('#slide').src.split('/');
+   		strUltimaImg = arrayImg[arrayImg.length-1];
+
+   		for (var i = 0 ; i < todasAsImagens.length; i++) {
+   			if(todasAsImagens[i] == strUltimaImg){
+   				anterior = i-1;
+   				if(anterior == -1) {
+   					strUltimaImg = todasAsImagens[todasAsImagens.length-1];
+   				} else {
+		   			strUltimaImg = todasAsImagens[anterior];
+	   			}
+   			}
+   		}
+
+		arrayImg = document.querySelector('#slide').src = servidorDasImagens + strUltimaImg;   		
+  });
+});
+
+document.querySelectorAll('#proximo').forEach(function(el) {
+   el.addEventListener('click', function(e) {
+   		arrayImg = document.querySelector('#slide').src.split('/');
+   		strUltimaImg = arrayImg[arrayImg.length-1];
+
+   		for (var i = 0 ; i < todasAsImagens.length; i++) {
+   			if(todasAsImagens[i] == strUltimaImg){
+   				proximo = i+1;
+   				if(proximo % todasAsImagens.length == 0) {
+   					strUltimaImg = todasAsImagens[0];
+   				} else {
+		   			strUltimaImg = todasAsImagens[proximo];
+	   			}
+	   			break;
+   			}
+   		}
+
+		arrayImg = document.querySelector('#slide').src = servidorDasImagens + strUltimaImg;   		
+  });
+});
